6f936e37c82a07abeb887dea365a2e509fb25130
Vijay Krishnamoorthy <vijayk0747@gmail.com>
2022-04-13 15:49:50 -0500

    initial js added

---
+++ script.js
@@ -0,0 +1,20 @@
+let firstOperand = ''
+let secondOperand = ''
+let currentOperation = null
+let shouldResetScreen = false
+
+function add(a, b) {
+    return a + b
+  }
+  
+  function substract(a, b) {
+    return a - b
+  }
+  
+  function multiply(a, b) {
+    return a * b
+  }
+  
+  function divide(a, b) {
+    return a / b
+  }
